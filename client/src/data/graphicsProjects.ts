e9f2c202ea219ffedeb40e8b7326e3822ba95f43
MeDevRob <118245001+JustRob12@users.noreply.github.com>
2026-02-08 22:04:10 +0800

    wow

--- client/src/data/graphicsProjects.ts
+++ client/src/data/graphicsProjects.ts
@@ -3,37 +3,37 @@ import type { Project } from '../types';
 export const graphicsProjects: Project[] = [
   {
     id: 'gfx-1',
-    image: 'public/picture/Graphics/1.jpeg',
+    image: '/picture/Graphics/1.jpeg',
     category: 'Graphics',
   },
   {
     id: 'gfx-2',
-    image: 'public/picture/Graphics/58th LOGO.png',
+    image: '/picture/Graphics/58th LOGO.png',
     category: 'Graphics',
   },
   {
     id: 'gfx-3',
-    image: 'publicpicture/Graphics/Buwan ng Wika.png',
+    image: '/picture/Graphics/Buwan ng Wika.png',
     category: 'Graphics',
   },
   {
     id: 'gfx-4',
-    image: 'public/picture/Graphics/INDEPENDENCE DAY.png',
+    image: '/picture/Graphics/INDEPENDENCE DAY.png',
     category: 'Graphics',
   },
   {
     id: 'gfx-5',
-    image: 'public/picture/Graphics/TARRAGONA TRAILBLAZERS.png',
+    image: '/picture/Graphics/TARRAGONA TRAILBLAZERS.png',
     category: 'Graphics',
   },
   {
     id: 'gfx-6',
-    image: 'public/picture/Graphics/TOP DAVAO ORIENTAL JOY.png',
+    image: '/picture/Graphics/TOP DAVAO ORIENTAL JOY.png',
     category: 'Graphics',
   },
   {
     id: 'gfx-7',
-    image: 'public/picture/Graphics/prelim4.png',
+    image: '/picture/Graphics/prelim4.png',
     category: 'Graphics',
   },
 ];
